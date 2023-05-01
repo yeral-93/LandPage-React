1f048fb286d6e2d214a27e3a8dc7185da5003406
linavaquero <lina.vaquero2017@gmail.com>
2023-05-01 15:50:28 -0500

    creacion de mainBanner

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainBanner from "../mainBanner/MainBanner";
+
+const Main = () => {
+  return (
+    <div>
+    <MainBanner/>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/mainBanner/MainBanner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainBanner = () => {
+  return (
+    <div>MainBanner</div>
+  )
+}
+
+export default MainBanner
