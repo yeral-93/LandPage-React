708ff98d31e72cea438ffa41c13619b3e21e2be4
linavaquero <lina.vaquero2017@gmail.com>
2023-05-04 15:55:55 -0500

    detalles de styles

--- src/components/mainBanner/StyledMainBanner.jsx
+++ src/components/mainBanner/StyledMainBanner.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StylesBanner = styled.div`
-padding: 60px 85px 0px 85px;
+padding: 70px 85px 0px 85px;
 `
 
 export const Informacion = styled.div`
