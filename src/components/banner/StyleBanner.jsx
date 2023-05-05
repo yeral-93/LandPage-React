e6ebbfe21fbf349b10577d6d8d6f50c20a380610
02Alexis <m_chael1012@hotmail.com>
2023-05-05 14:35:21 -0500

    finalizado diseño mobile del header

--- src/components/banner/StyleBanner.jsx
+++ src/components/banner/StyleBanner.jsx
@@ -3,16 +3,14 @@ import ImgB from '../../img/desktop/image-hero.jpg'
 import ImgMobile from '../../img/mobile/image-hero.jpg'
 
 export const StyleBa = styled.div`
-    background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
-     url(${ImgB});
+    background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)), url(${ImgB});
     min-height: 500px;
     height: 100vh;
     background-position: center;
     background-size: cover;
 
     @media screen and (max-width: 375px) {
-        background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
-         url(${ImgMobile});
+        background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)), url(${ImgMobile});
         min-height: 500px;
         height: 100vh;
         background-position: center;
@@ -44,6 +42,42 @@ export const StyleItems = styled.div`
 `;
 
 export const StyleH1 = styled.h1`
+  width: 660px;
+  font-size: 4.5rem;
+  line-height: 4rem;
+  color: var(--White);
+  border: 2.3px solid var(--White);
+  letter-spacing: 5px;
+  padding: 30px;
+
+  @media screen and (max-width: 375px) {
+    width: 100%;
+    font-size: 2.2rem;
+    line-height: 2.3rem;
+    letter-spacing: 0.1rem;
+  }
+`;
+
+/* 
+
+export const StyleItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100vh;
+  max-height: 100%;
+  padding: 0 2rem;
+  width: 650px;
+  text-transform: uppercase;
+  line-height: 1;
+  font-weight: bold;
+  @media screen and (max-width: 375px) {
+    width: 100%;
+  }
+`;
+
+export const StyleH1 = styled.h1`
   width: 630px;
   font-size: 4.3rem;
   line-height: 4rem;
@@ -56,5 +90,11 @@ export const StyleH1 = styled.h1`
 
   @media screen and (max-width: 375px) {
     margin: 0;
+    width: 100%;
+    font-size: 2.1rem;
+    line-height: 2.1rem;
+    letter-spacing: 0.1rem;
+    transform: translateY(-35%);
   }
 `;
+*/
--- src/components/navigationBar/styleNavigationBar.jsx
+++ src/components/navigationBar/styleNavigationBar.jsx
@@ -1,61 +1,186 @@
 import styled from "styled-components"
 
-export const Nav = styled.nav`
-    background: transparent;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 55px 85px 10px 85px;
-    ul{
-        display: flex;
-        gap: 26px;
-        align-items: center;
-    }
-    li{
-        position: relative;
-        font-size: 'Alata';
-        color: var(--White);
-        cursor: pointer;
-        ::before{
-            content: "";
-            background-color: var(--White);
-            position: absolute;
-            left: 0.6rem;
-            bottom: -0.6rem;
-            align-items: center;
-            height: 2px;
-            width: 0;
-            transition: 0.3s ease-in-out;
-        }
-        :hover::before{
-            content: "";
-            background-color: var(--White);
-            position: absolute;
-            left: 0.6rem;
-            bottom: -0.6rem;
-            height: 3px;
-            width: 75%;
-        }
-    }
-    @media screen and (max-width: 375px) {
-        display: none;
-    }
-`
-export const Mobileicon = styled.figure`
-    display: none;
+export const Container = styled.div`
 
-    @media screen and (max-width: 375px) {
-        display: flex;
-        align-items: center;
-        cursor: pointer;
-        img{
-            margin: 0.5rem;
-        }
-    }
 `
-// @media screen and (max-width: 375px) {
-// display: grid;
-// justify-content: space-between;
-// align-items: center;
-// grid-template-columns: repeat(2, max-content);
-// }
+
+export const Nav = styled.nav`
+margin: 0 auto;
+width: 90%;
+max-width: 1200px;
+overflow: hidden;
+
+--state-close: scale(0); /* no se va a mostrar */
+--state-hamburguer: scale(1);  /* se va a mostrar */
+--state-menu: translate(-100%);
+padding: 40px 0;
+display: grid;
+z-index: 1;
+justify-content: space-between;
+align-items: center;
+grid-template-columns: repeat(2, max-content);
+:target{
+    --state-menu: translate(0);
+    --state-close: scale(1);
+    --state-hamburguer: scale(0);
+}
+img{
+    font-size: 150px;
+    @media (min-width: 768px) {
+        font-size: 10.5rem;
+    }
+}
+
+@media (min-width: 768px) {
+    padding: 55px 0 10px 0;
+
+    --state-hamburguer: scale(0);
+    --state-menu: translate(0);
+
+    :target{
+    --state-close: scale(0);
+}
+}
+`
+
+export const StyleLinks = styled.ul`
+background-color: var(--Black);
+position: absolute;
+top: 0;
+bottom: 0;
+left: 0;
+right: 0;
+
+display: grid;
+align-content: center;
+gap: 2em;
+padding-left: 5%;
+
+transform: var(--state-menu);
+transition: .3s transform;
+li{
+color: var(--White);
+font-size: 1.6rem;
+text-transform: uppercase;
+letter-spacing: 2px;
+
+position: relative;    
+    cursor: pointer;
+    ::before{
+        content: "";
+        background-color: var(--White);
+        position: absolute;
+        left: 0.6rem;
+        bottom: -0.6rem;
+        align-items: center;
+        height: 2px;
+        width: 0;
+        transition: 0.3s ease-in-out;
+    }
+    :hover::before{
+        content: "";
+        background-color: var(--White);
+        position: absolute;
+        left: 0.6rem;
+        bottom: -0.6rem;
+        height: 3px;
+        width: 75%;
+    }
+@media (min-width: 768px) {
+    font-size: 1rem;
+    letter-spacing: none;    
+}
+}
+
+@media (min-width: 768px) {
+    background-color: unset;
+    position: unset;
+    padding: 0;
+    gap: 1.5em;
+    grid-auto-flow: column;
+
+    transform: unset;
+
+    grid-column: -2/-1;
+    grid-row: 1/2;
+}
+`
+
+export const Hamburguer = styled.a`
+cursor: pointer;
+grid-column: -2/-1;
+grid-row: 1/2;
+
+transform: var(--state-hamburguer);
+transition: .4s transform;
+img{
+    width: 30px;
+}
+`
+
+export const Close = styled.a`
+cursor: pointer;
+grid-column: -2/-1;
+grid-row: 1/2;
+
+transform: var(--state-close);
+transition: .4s transform;
+img{
+    width: 40px;
+}
+`
+
+// export const Nav = styled.nav`
+//     background: transparent;
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 55px 85px 10px 85px;
+//     ul{
+//         display: flex;
+//         gap: 26px;
+//         align-items: center;
+        
+//         @media screen and (max-width: 375px) {
+//         display: none;
+//     }
+//     }
+//     li{
+//         position: relative;
+//         font-size: 'Alata';
+//         color: var(--White);
+//         cursor: pointer;
+//         ::before{
+//             content: "";
+//             background-color: var(--White);
+//             position: absolute;
+//             left: 0.6rem;
+//             bottom: -0.6rem;
+//             align-items: center;
+//             height: 2px;
+//             width: 0;
+//             transition: 0.3s ease-in-out;
+//         }
+//         :hover::before{
+//             content: "";
+//             background-color: var(--White);
+//             position: absolute;
+//             left: 0.6rem;
+//             bottom: -0.6rem;
+//             height: 3px;
+//             width: 75%;
+//         }
+//     }
+// `
+// export const Mobileicon = styled.figure`
+//     display: none;
+
+//     @media screen and (max-width: 375px) {
+//         display: flex;
+//         align-items: center;
+//         cursor: pointer;
+//         img{
+//             margin: 0.5rem;
+//         }
+//     }
+// `
