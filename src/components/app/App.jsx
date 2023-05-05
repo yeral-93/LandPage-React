94d3aa3801cf073b4bddb65ad810fed7f2634586
02Alexis <m_chael1012@hotmail.com>
2023-05-05 14:43:05 -0500

    corrigiendo detalles

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from "../header/Header"
 import Banner from "../banner/Banner";
 import GlobalStyles from './StyleApp';
 import Footer from '../footer/Footer';
